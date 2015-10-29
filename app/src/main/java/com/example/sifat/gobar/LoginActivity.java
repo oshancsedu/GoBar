2657b674b8a0196fa34faa9272ea4c6adb911e6f
oshancsedu <sifat.oshan@gmail.com>
2015-10-29 20:39:32 +0600

    Trying to implement login with facebook(problem with hashkey)

--- app/src/main/java/com/example/sifat/gobar/LoginActivity.java
+++ app/src/main/java/com/example/sifat/gobar/LoginActivity.java
@@ -1,16 +1,97 @@
 package com.example.sifat.gobar;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 /**
  * Created by Sifat on 10/28/2015.
  */
 public class LoginActivity extends ActionBarActivity {
 
+    private LoginButton loginButton;
+    private FacebookCallback<LoginResult> facebookCallback;
+    private CallbackManager callbackManager;
+    private List<String> permission;
+    private Profile profile;
+    private AccessToken accessToken;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this);
         setContentView(R.layout.login);
+        init();
+        facebookCallback = new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                accessToken = loginResult.getAccessToken();
+                profile = Profile.getCurrentProfile();
+                if (profile != null) {
+                    Toast.makeText(LoginActivity.this, "Profile name : " + profile.getName(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "ID : " + profile.getId(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Frist name : " + profile.getFirstName(), Toast.LENGTH_SHORT).show();
+                }
+            }
 
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        };
+    }
+
+    private void init() {
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.btFBLogin);
+        //permission.add("user_friend");
+        //loginButton.setReadPermissions("user_friend");
+        loginButton.registerCallback(callbackManager, facebookCallback);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
     }
 }
